e3ace46f0f428b42a6e97dafa3e24bc3afeeec8d
Android Studio <devandroid@ort>
2022-04-07 10:37:16 -0300

    adding dai

--- dai/ejercicios/01.js
+++ dai/ejercicios/01.js
@@ -2,12 +2,27 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-const process_data = (datos) => {
+const process_data = () => {
 
-    let x = 100;
+  const data = [11, 9, 2, -1, 110, 99, 8];
+
+  let odds = [];
+  
+  for (let i = 0; i < data.length; i++) {
+      const element = data[i];
+  
+      if (element % 2 !== 0) {
+          odds.push(element);
+      }
+      
+  }
+  
+  const proc = odds.sort().reverse();
+  
+  console.log(proc[1]);
 
     return {
-        resultado: x
+        resultado: proc[1]
     };
 };
 
