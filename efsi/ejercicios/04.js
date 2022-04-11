24dcc71e2552af94f0a2dc3b437236beb441ea6d
Android Studio <devandroid@ort>
2022-04-11 10:46:14 -0300

    EJ3,4

---
+++ efsi/ejercicios/04.js
@@ -0,0 +1,2 @@
+// Calcula los años bisiestos que habrá desde ahora y 
+// antes de 2150
---
+++ efsi/ejercicios/05.js
@@ -0,0 +1,6 @@
+// ¿Cuántas cartas con número par y negras hay en una 
+// baraja de poker?
+
+// Eliminar 6 cartas de la baraja al azar y calcular en promedio
+// (haciendo 1000 iteraciones) cuanto valen las cartas (con número)
+// que quedan.
